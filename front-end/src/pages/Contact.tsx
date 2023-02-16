abef00264aa2d1b1d0fe8f712ac43cacccf0c1b6
Thiago Dias <thiaguinhodias.15@gmail.com>
2023-02-16 16:05:24 -0300

    feat(front-end): footer section

--- front-end/src/pages/Contact.tsx
+++ front-end/src/pages/Contact.tsx
@@ -1,16 +1,26 @@
 import React from 'react';
+import { ImLinkedin2 } from "react-icons/im";
+import { ImTwitter } from "react-icons/im";
+import { ImInstagram } from "react-icons/im";
 
 export default function Form() {
     return (
-        <main>
-            <h1>Reach out to us!</h1>
-            <form method="POST">
-                <input placeholder="Your name*" />
-                <input placeholder="Your e-mail*" />
-                <textarea placeholder="Your message*"></textarea>
-                
-                <button type="submit">Send message</button>
-            </form>
-        </main>
+        <>
+            <main>
+                <h1>Reach out to us!</h1>
+                <form method="POST">
+                    <input placeholder="Your name*" />
+                    <input placeholder="Your e-mail*" />
+                    <textarea placeholder="Your message*"></textarea>
+                    
+                    <button type="submit">Send message</button>
+                </form>
+            </main>
+            <footer>
+                <ImLinkedin2 />
+                <ImTwitter />
+                <ImInstagram />
+            </footer>
+        </>
     );
 }
