20e4504a61f557d2a0a9e4664033f8d261cf317b
Thiago Dias <thiaguinhodias.15@gmail.com>
2023-02-16 18:13:01 -0300

    feat(front-end): saving name, e-mail and message f from form

--- front-end/src/pages/Contact.tsx
+++ front-end/src/pages/Contact.tsx
@@ -1,18 +1,39 @@
-import React from 'react';
+import React, { FormEvent, useState } from 'react';
 import { ImLinkedin2 } from "react-icons/im";
 import { ImTwitter } from "react-icons/im";
 import { ImInstagram } from "react-icons/im";
 import Map from '../assets/map.svg';
 
 export default function Contact() {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+
+    const validateForm = (e: FormEvent) => {
+        e.preventDefault();
+    }
+
     return (
         <>
             <main>
                 <h1>Reach out to us!</h1>
-                <form method="POST">
-                    <input placeholder="Your name*" />
-                    <input placeholder="Your e-mail*" />
-                    <textarea placeholder="Your message*"></textarea>
+                <form method="POST" onSubmit={(e) => validateForm(e)}>
+                    <input 
+                        placeholder="Your name*" 
+                        value={name} onChange={({ target }) => setName(target.value)} 
+                        required
+                    />
+                    <input
+                        placeholder="Your e-mail*" 
+                        value={email} onChange={({ target }) => setEmail(target.value)} 
+                        required
+                    />
+                    <textarea 
+                        placeholder="Your message*" 
+                        value={message} 
+                        onChange={({ target }) => setMessage(target.value)} 
+                        required
+                    ></textarea>
                     
                     <button type="submit">Send message</button>
                 </form>
