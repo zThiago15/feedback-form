8d371d775b5abefa74a0f977fd5df42ada6d7d8a
Thiago Dias <thiaguinhodias.15@gmail.com>
2023-02-17 18:46:52 -0300

    refactor(front-end): change baseUrl to connect with API

--- front-end/src/services/index.ts
+++ front-end/src/services/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3002',
+    baseURL: 'https://feedbackform-back.up.railway.app/',
 });
 
 export default api;
