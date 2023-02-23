d3ca9304db1ca4f205ad55ae4a6f6773b2674225
Thiago Dias <thiaguinhodias.15@gmail.com>
2023-02-23 19:14:42 -0300

    fix: remove package.json fiin root directory

--- front-end/src/redux/store.ts
+++ front-end/src/redux/store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
+import contactReducer from './contactSlicer';
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    contact: contactReducer
+  },
 })
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
