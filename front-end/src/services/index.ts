cde8868dcf1cdcb39fdb8c118fe89e21607c3047
Thiago Dias <thiaguinhodias.15@gmail.com>
2023-02-24 01:33:42 -0300

    fix: remove '/' in baseUrl

--- front-end/src/services/index.ts
+++ front-end/src/services/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://feedbackform-back.up.railway.app/',
+    baseURL: 'https://feedbackform-back.up.railway.app',
 });
 
 export default api;
