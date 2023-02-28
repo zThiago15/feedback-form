25dc4dcec5f34dec251641787bc27c1a76d602c2
Thiago Dias <thiaguinhodias.15@gmail.com>
2023-02-28 03:04:52 +0000

    docs: swagger config

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,6 +1,8 @@
 import express, { Request, Response } from 'express';
 import feedbackRoute from './routes/feedbackRoutes';
 import cors from 'cors';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDoc from './swagger/swagger.json';
 
 const app = express();
 
@@ -8,8 +10,11 @@ app.use(cors());
 app.use(express.json());
 app.use(feedbackRoute);
 
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
+
 app.get('/', (_req: Request, res: Response) => {
-    return res.status(200).send('Welcome to server!');
+    return res.status(200).send('Welcome to server');
 });
 
 const port = process.env.PORT || 3000;
